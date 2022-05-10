4221432d5e48d99aa04a8976b6b09a2ed9b118b5
Martin Angers <martin.n.angers@gmail.com>
2022-05-10 13:04:41 -0400

    Missed save

--- client_test.go
+++ client_test.go
@@ -76,29 +76,4 @@ func TestUpstash(t *testing.T) {
 		require.Equal(t, "ERR", rerr.Kind)
 		require.Equal(t, 0, rerr.PipelineIndex)
 	})
-
-	/*
-		t.Run("pipeline Send Flush Receive", func(t *testing.T) {
-			err := conn.Send("SET", "a", 1)
-			require.NoError(t, err)
-			err = conn.Send("APPEND", "a", "2")
-			require.NoError(t, err)
-			err = conn.Send("INCR", "a")
-			require.NoError(t, err)
-			err = conn.Flush()
-			require.NoError(t, err)
-
-			res1, err := redis.String(conn.Receive())
-			require.NoError(t, err)
-			require.Equal(t, "OK", res1)
-
-			res2, err := redis.Int(conn.Receive())
-			require.NoError(t, err)
-			require.Equal(t, 2, res2)
-
-			res3, err := redis.Int(conn.Receive())
-			require.NoError(t, err)
-			require.Equal(t, 13, res3)
-		})
-	*/
 }
